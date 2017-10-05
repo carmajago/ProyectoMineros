0f16a8d43b7df03180a1826e32245b67559df68b
Carlos <carlos.1701412853@ucaldas.edu.co>
2017-10-04 21:04:02 -0500

    Revisar modelo json

---
+++ Assets/Scripts/ModeloJson.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class ModeloJson : MonoBehaviour {
+
+    public int mineros_oro;
+    public int mineros_plata;
+    public int mineros_cobre;
+    public int mineros_comodin;
+
+    public Dictionary<int, Dictionary<int, Dictionary<int, string>>> mapas;
+   
+}
